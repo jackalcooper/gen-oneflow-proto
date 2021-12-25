e530e6cebab4d07925a6cc2eca5d5318835fcd74
jackalcooper <jackalcooper@gmail.com>
2021-12-25 12:03:54 +0000

    refine

--- gen-td.cpp
+++ gen-td.cpp
@@ -136,7 +136,7 @@ void ConverFields(const google::protobuf::Descriptor *d,
       if (t->name() == "ShapeProto") {
         ods_def.add_attr("ShapeAttr:$" + f->name());
       } else if (t->name() == "Int64List") {
-        std::cerr << "SI64ArrayAttr";
+        ods_def.add_input("SI64ArrayAttr:$" + f->name());
       } else if (t->name() == "LogicalBlobId") {
         ods_def.add_input("OneFlow_Tensor:$" + f->name());
       } else {
@@ -170,15 +170,7 @@ bool MyCodeGenerator::Generate(const FileDescriptor *file,
                                const std::string &parameter,
                                GeneratorContext *generator_context,
                                std::string *error) const {
-  std::cerr << parameter << "\n";
-  std::cerr << *error << "\n";
-  std::cerr << file->name() << "\n";
-  // for (int i; i < file->message_type_count(); i++) {
-  //   auto m = file->message_type(i);
-  //   std::cerr << m->name() << "\n";
-  // }
   auto OperatorConf = file->FindMessageTypeByName("OperatorConf");
-  std::cerr << OperatorConf->name() << "\n";
   auto op_type = OperatorConf->FindOneofByName("op_type");
   for (int filed_i; filed_i < op_type->field_count(); filed_i += 1) {
     auto m = op_type->field(filed_i);
